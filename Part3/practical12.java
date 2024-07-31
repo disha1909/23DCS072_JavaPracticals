c15172d7dec49b3c9ff47674448bbba158c11173
disha1909 <dp3815214@gmail.com>
2024-07-31 13:54:52 +0530

    Create practical12.java

---
+++ Part3/practical12.java
@@ -0,0 +1,11 @@
+
+ class practical12{
+    public static void main(String args[]){
+        System.out.println("In rupees:" +args[0]);
+        int a =Integer.parseInt(args[0]);
+
+
+        int b=a*100;
+        System.out.println("In pound:"+ b);
+    }
+}
