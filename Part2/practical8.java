19078bda921fb3df2f4b2604340fcbfd78a70b7a
disha1909 <dp3815214@gmail.com>
2024-07-31 13:45:21 +0530

    Create practical8.java

---
+++ Part2/practical8.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+import java.util.Scanner;
+
+class practical8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Number of 9's in the array: " + array_count9(array));
+
+        scanner.close();
+    }
+
+    public static int array_count9(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (num == 9) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
